693019e4ae15d1e1d37d80ba926e53baf0956b97
DLourenco18 <contact.lourenco18@gmail.com>
2025-10-15 09:59:17 +0100

    a

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dasantos <dasantos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 11:25:27 by dasantos          #+#    #+#             */
-/*   Updated: 2025/10/14 14:39:30 by dasantos         ###   ########.fr       */
+/*   Updated: 2025/10/14 18:51:02 by dasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,30 +30,26 @@ void ft_bzero(void *s, size_t n); // done
 void *ft_memcpy(void *dst, const void *src, size_t n);//done 
 void *ft_memmove(void *dst, const void *src, size_t len);//done
 void *ft_calloc(size_t nmemb, size_t size);
+void *ft_memchr(const void *s, int c, size_t n);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
 
 //string functions
 size_t ft_strlen(const char *s);// done
 size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);// done
 size_t ft_strlcat(char *dst, const char *src, size_t dstsize); //done
-
-// to.. functions
-int ft_toupper(int c);
-int ft_tolower(int c);
-
-
 char *ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
-
-
-void *ft_memchr(const void *s, int c, size_t n);
-int ft_memcmp(const void *s1, const void *s2, size_t n);
 char *ft_strnstr(const char *haystack, const char *needle, size_t len);
+char *ft_strdup(const char *s1);
 
-
+// to.. functions
+int ft_toupper(int c);//done
+int ft_tolower(int c);// done 
+// transform functions
 int ft_atoi(const char *str);
 
-char *ft_strdup(const char *s1);
+
 
 
 #endif
