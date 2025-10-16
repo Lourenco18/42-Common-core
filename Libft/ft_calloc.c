242cb9d1668ee4be6787dd50edb2851b80af1390
DLourenco18 <contact.lourenco18@gmail.com>
2025-10-16 12:38:10 +0100

    all correct and funcional

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,18 +6,19 @@
 /*   By: dasantos <dasantos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 12:18:25 by dasantos          #+#    #+#             */
-/*   Updated: 2025/10/16 12:35:27 by dasantos         ###   ########.fr       */
+/*   Updated: 2025/10/16 12:38:01 by dasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_calloc(int nmemb, unsigned int size)
+void *ft_calloc(unsigned int nmemb, unsigned int size)
 {
     void *ptr;
-    if (nmemb && size && nmemb > SIZE_MAX / size)
+
+    if (nmemb && size && nmemb > 4294967295U / size)
         return (0);
     ptr = malloc(nmemb * size);
     if (!ptr)
         return (0);
     ft_bzero(ptr, nmemb * size);
-    return ptr;
+    return (ptr);
 }
