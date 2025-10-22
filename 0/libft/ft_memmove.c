38b9e695e5c1a16c17557b5c649c75b6d789bda7
DLourenco18 <contact.lourenco18@gmail.com>
2025-10-22 10:45:26 +0100

    all done

--- 0/libft/ft_memmove.c
+++ 0/libft/ft_memmove.c
@@ -6,15 +6,15 @@
 /*   By: dasantos <dasantos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 00:01:59 by hroxo             #+#    #+#             */
-/*   Updated: 2025/10/22 10:39:05 by dasantos         ###   ########.fr       */
+/*   Updated: 2025/10/22 10:45:22 by dasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void *ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
 	if (!dest && !src)
@@ -37,20 +37,20 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 #include <string.h>
 #include <stdio.h>
 
-int	main(void)
+int main(void)
 {
-	char	str[100] = "Learningisfun";
-	char	str1[100] = "Learningisfun";
-
+	char str[100] = "Learningisfun";
 	char *first, *second;
+
 	first = str;
 	second = str;
 	memmove(second + 8, first, 10);
 	printf("==>memmove original <==\n%s\n", str);
+	char str1[100] = "Learningisfun";
 	first = str1;
 	second = str1;
 	ft_memmove(second + 8, first, 10);
 	printf("==>memmove meu <==\n%s\n", str1);
-	return (0);
+	return 0;
 }
 */
