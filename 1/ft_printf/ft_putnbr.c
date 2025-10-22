559632b1dad31e774f94b1a78fb93782332a7903
DLourenco18 <contact.lourenco18@gmail.com>
2025-10-22 12:37:40 +0100

    all functions done

--- 1/ft_printf/ft_putnbr.c
+++ 1/ft_printf/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: dasantos <dasantos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 11:28:57 by dasantos          #+#    #+#             */
-/*   Updated: 2025/10/22 12:27:46 by dasantos         ###   ########.fr       */
+/*   Updated: 2025/10/22 12:36:20 by dasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,3 @@ void ft_putnbr_fd(int n, int fd)
 		ft_putnbr(num / 10);
 	ft_putchar((num % 10) + '0');
 }
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	ft_putnbr_fd(42, 1);
-	return (0);
-}
-*/
