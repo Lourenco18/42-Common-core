3fbbea278f11939cfdf2e58fc5b98671dfb52c64
DLourenco18 <contact.lourenco18@gmail.com>
2025-10-22 10:58:51 +0100

    ..

--- 0/libft/ft_lstadd_front_bonus.c
+++ 0/libft/ft_lstadd_front_bonus.c
@@ -5,17 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dasantos <dasantos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/02 00:19:39 by dasantos             #+#    #+#             */
-/*   Updated: 2025/10/22 10:20:10 by dasantos         ###   ########.fr       */
+/*   Created: 2025/10/22 10:57:24 by dasantos          #+#    #+#             */
+/*   Updated: 2025/10/22 10:57:27 by dasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
-		return;
+		return ;
 	new->next = *lst;
 	*lst = new;
 }
