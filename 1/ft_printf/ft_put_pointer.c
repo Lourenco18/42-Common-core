15d89166b7d7d9bb444e987cc5ad5b930c193160
DLourenco18 <contact.lourenco18@gmail.com>
2025-10-23 11:20:33 +0100

    all done

--- 1/ft_printf/ft_put_pointer.c
+++ 1/ft_printf/ft_put_pointer.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putptr.c                                        :+:      :+:    :+:   */
+/*   ft_put_pointer.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dasantos <dasantos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 11:11:34 by dasantos          #+#    #+#             */
-/*   Updated: 2025/10/23 11:19:29 by dasantos         ###   ########.fr       */
+/*   Updated: 2025/10/23 11:19:58 by dasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int print_ptr_hex(unsigned long n)
+static int print_pointer(unsigned long n)
 {
 	char *base;
 	int count;
@@ -20,7 +20,7 @@ static int print_ptr_hex(unsigned long n)
 	base = "0123456789abcdef";
 	count = 0;
 	if (n >= 16)
-		count += print_ptr_hex(n / 16);
+		count += print_pointer(n / 16);
 	count += ft_putchar(base[n % 16]);
 	return (count);
 }
@@ -34,6 +34,6 @@ int ft_put_pointer(void *ptr)
 		return (write(1, "(nil)", 5));
 	address = (unsigned long)ptr;
 	count = write(1, "0x", 2);
-	count += print_ptr_hex(address);
+	count += print_pointer(address);
 	return (count);
 }
