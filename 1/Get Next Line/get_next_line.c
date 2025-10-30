81e13ca3c5cb4d890355014f5c66587f9a8bd59b
DLourenco18 <contact.lourenco18@gmail.com>
2025-10-30 11:52:05 +0000

    almost done

--- 1/Get Next Line/get_next_line.c
+++ 1/Get Next Line/get_next_line.c
@@ -5,45 +5,89 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dasantos <dasantos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/28 09:31:07 by dasantos          #+#    #+#             */
-/*   Updated: 2025/10/29 09:33:16 by dasantos         ###   ########.fr       */
+/*   Created: 2025/10/29 11:39:08 by dasantos          #+#    #+#             */
+/*   Updated: 2025/10/30 11:48:43 by dasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+static char	*join_and_free(char *s1, char *s2)
 {
-    char *line;
-    ssize_t bytes_read;
+	char	*temp;
 
-    line = malloc(256);
-    if (!line)
-        return (NULL);
-    bytes_read = read(fd, line, 255);
-    if (bytes_read <= 0)
-    {
-        free(line);
-        return (NULL);
-    }
-    line[bytes_read] = '\0';
-    return (line);
+	temp = ft_strjoin(s1, s2);
+	free(s1);
+	return (temp);
 }
 
-int main(void)
+static char	*read_and_stash(int fd, char *backup)
 {
-    FILE *file = fopen("file.txt", "r");
-    if (file)
-    {
-        int fd = fileno(file);
-        char *line;
-        while ((line = get_next_line(fd)))
-        {
-            printf("%s", line);
-            free(line);
-        }
-        fclose(file);
+	char	*past;
+	ssize_t	bytes_read;
 
-        return (0);
-    }
+	if (!backup)
+		backup = ft_strdup("");
+	past = malloc(BUFFER_SIZE + 1);
+	if (!past)
+		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr(backup, '\n') && bytes_read > 0)
+	{
+		bytes_read = read(fd, past, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (free(past), free(backup), NULL);
+		past[bytes_read] = '\0';
+		backup = join_and_free(backup, past);
+		if (!backup)
+			return (free(past), NULL);
+	}
+	free(past);
+	return (backup);
+}
+
+static char	*save_remainder(char *stash)
+{
+	int		i;
+	int		j;
+	char	*remainder;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+		return (free(stash), NULL);
+	remainder = malloc(ft_strlen(stash) - i);
+	if (!remainder)
+		return (NULL);
+	i++;
+	j = 0;
+	while (stash[i])
+		remainder[j++] = stash[i++];
+	remainder[j] = '\0';
+	free(stash);
+	return (remainder);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*backup;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	backup = read_and_stash(fd, backup);
+	if (!backup)
+		return (NULL);
+	line = extract_line(backup);
+	backup = save_remainder(backup);
+	if (!line || !*line)
+	{
+		free(backup);
+		backup = NULL;
+		if (line)
+			free(line);
+		return (NULL);
+	}
+	return (line);
 }
--- 1/Get Next Line/get_next_line_utils.c
+++ 1/Get Next Line/get_next_line_utils.c
@@ -6,7 +6,104 @@
 /*   By: dasantos <dasantos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 11:34:02 by dasantos          #+#    #+#             */
-/*   Updated: 2025/10/29 09:33:18 by dasantos         ###   ########.fr       */
+/*   Updated: 2025/10/30 11:37:11 by dasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strdup(const char *src)
+{
+	int		i;
+	int		len;
+	char	*dup;
+
+	len = ft_strlen((char *)src);
+	dup = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (0);
+	i = 0;
+	while (src[i])
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	size;
+
+	size = 0;
+	while (s[size])
+	{
+		size++;
+	}
+	return (size);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	ch;
+
+	ch = (unsigned char)c;
+	while (*s)
+	{
+		if ((unsigned char)*s == ch)
+			return ((char *)s);
+		s++;
+	}
+	if (ch == '\0')
+		return ((char *)s);
+	return (0);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+	int		len1;
+	int		len2;
+	int		i;
+
+	if (!s1 || !s2)
+		return (0);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	joined = (char *)malloc(len1 + len2 + 1);
+	if (!joined)
+		return (0);
+	i = 0;
+	while (*s1)
+		joined[i++] = *s1++;
+	while (*s2)
+		joined[i++] = *s2++;
+	joined[i] = '\0';
+	return (joined);
+}
+
+char	*extract_line(char *backup)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!backup || !backup[0])
+		return (NULL);
+	while (backup[i] && backup[i] != '\n')
+		i++;
+	line = malloc(i + (backup[i] == '\n') + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (backup[i] && backup[i] != '\n')
+	{
+		line[i] = backup[i];
+		i++;
+	}
+	if (backup[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
