ed637f9a8afed660c48cf5ace01738634e01ecc0
DLourenco18 <contact.lourenco18@gmail.com>
2025-11-03 11:04:21 +0000

    done but with lib functions

--- 1/Get Next Line/get_next_line.h
+++ 1/Get Next Line/get_next_line.h
@@ -6,27 +6,27 @@
 /*   By: dasantos <dasantos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 11:48:16 by dasantos          #+#    #+#             */
-/*   Updated: 2025/10/30 11:48:17 by dasantos         ###   ########.fr       */
+/*   Updated: 2025/11/02 18:27:57 by dasantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
-# include <stddef.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42
+#endif
 
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *src);
-char	*get_next_line(int fd);
-size_t	ft_strlen(const char *s);
-char	*ft_strjoin(const char *s1, const char *s2);
-char	*extract_line(char *s);
+char *ft_strchr(const char *s, int c);
+char *ft_strdup(const char *src);
+char *get_next_line(int fd);
+size_t ft_strlen(const char *s);
+char *ft_strjoin(const char *s1, const char *s2);
+char *extract_line(char *s);
 
 #endif
